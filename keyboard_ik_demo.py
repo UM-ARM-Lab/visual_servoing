765cade8c82080fc6b4c9cb369377f4047160afe
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-01-18 21:42:49 -0500

    clean up IK demo

--- keyboard_ik_demo.py
+++ keyboard_ik_demo.py
@@ -1,78 +1,44 @@
+# Control end effector position with keyboard
+
 from src.utils import draw_pose, erase_pos
 from src.val import *
 from src.pbvs import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-# Control end effector position with keyboard
 
-
+# Key bindings
 KEY_U = 117
 KEY_I = 105
-KEY_O = 111
-KEY_P = 112
-KEY_H = 104
-KEY_J = 106
-KEY_K = 107
-KEY_L = 108
 KEY_UP = 65297
 KEY_RIGHT = 65296
 KEY_DOWN = 65298
 KEY_LEFT = 65295
 
+# Val robot and PVBS controller
 val = Val()
+pvbs = PVBS()
 p.setRealTimeSimulation(1)
 
+# create an initial target to do IK too
 perturb = np.zeros((6)) 
 perturb[0:3] = 0.1
 target = val.get_eef_pos("left") + perturb
-
-#target_marker = draw_sphere_marker(target[0:3], 0.01, (1.0, 0.0, 0.0, 1.0))
 marker = draw_pose(target[0:3], p.getQuaternionFromEuler(target[3:6]))
 
 
-#camera 
-projectionMatrix = p.computeProjectionMatrixFOV(
-        fov=45.0,
-        aspect=1.0,
-        nearVal=0.1,
-        farVal=3.1)
-viewMatrix = p.computeViewMatrix(
-    cameraEyePosition=[-2, 0, 0],
-    cameraTargetPosition=[0, 0, 0],
-    cameraUpVector=[0, 0, 1])
-width, height, rgbImg, depthImg, segImg = p.getCameraImage(
-    width=1000,
-    height=1000,
-    viewMatrix=viewMatrix,
-    projectionMatrix=projectionMatrix)
-rgb_img = np.array(rgbImg)[:, :, :3]
-depth_img = np.array(depthImg)
-print(rgb_img[:, :, 0])
-detect_markers(np.copy(rgb_img))
-#plt.imshow(rgb_img)
-#plt.show()
-#plt.figure()
-
-
 while(True):
+    # Move target marker based on updated target position
     marker_new = draw_pose(target[0:3], p.getQuaternionFromEuler(target[3:6]))
     erase_pos(marker)
     marker = marker_new
 
-    width, height, rgbImg, depthImg, segImg = p.getCameraImage(
-    width=1000,
-    height=1000,
-    viewMatrix=viewMatrix,
-    projectionMatrix=projectionMatrix)
-    rgb_img = np.array(rgbImg)[:, :, :3]
-    depth_img = np.array(depthImg)
-    print(rgb_img[:, :, 0])
-    detect_markers(np.copy(rgb_img))
+    # Get camera feed and detect markers
+    rgb, depth = pvbs.get_static_camera_img()
+    pvbs.detect_markers(np.copy(rgb))
     cv2.waitKey(10)
 
-
+    # Process keyboard to adjust target position
     events = p.getKeyboardEvents()
-    print(events)
     if(KEY_LEFT in events):
         target += np.array([0.01, 0.0, 0.0, 0.0, 0.0, 0.0])
     elif(KEY_RIGHT in events):
@@ -83,6 +49,5 @@ while(True):
     elif(KEY_DOWN in events):
         target -= np.array([0.00, 0.00, 0.01, 0.0, 0.0, 0.0])
 
+    # IK controller to target
     val.psuedoinv_ik("left", target, val.get_eef_pos("left"))
-
-input()
--- src/pbvs.py
+++ src/pbvs.py
@@ -1,43 +1,65 @@
 import cv2
+import pybullet as p
+import numpy as np
 
-def detect_markers(frame):
-    dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-    aruco_params = cv2.aruco.DetectorParameters_create()
+class PVBS:
 
-    (corners, ids, rejected) = cv2.aruco.detectMarkers(
-    frame, dict, parameters=aruco_params)
-    
-    if(len(corners) > 0):
-        # Flatten the ArUco IDs list
-        ids = ids.flatten()
+    def get_static_camera_img(self): 
+        # TODO replace magic numbers with class params
+        projectionMatrix = p.computeProjectionMatrixFOV(
+            fov=45.0,
+            aspect=1.0,
+            nearVal=0.1,
+            farVal=3.1)
+        viewMatrix = p.computeViewMatrix(
+            cameraEyePosition=[-2, 0, 0],
+            cameraTargetPosition=[0, 0, 0],
+            cameraUpVector=[0, 0, 1])
+        width, height, rgbImg, depthImg, segImg = p.getCameraImage(
+            width=1000,
+            height=1000,
+            viewMatrix=viewMatrix,
+            projectionMatrix=projectionMatrix)
+        rgb_img = np.array(rgbImg)[:, :, :3]
+        depth_img = np.array(depthImg)
+        return rgb_img, depth_img
+
+    def detect_markers(self, frame):
+        dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+        aruco_params = cv2.aruco.DetectorParameters_create()
+
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(
+        frame, dict, parameters=aruco_params)
         
-        # Loop over the detected ArUco corners
-        for (marker_corner, marker_id) in zip(corners, ids):
+        if(len(corners) > 0):
+            # Flatten the ArUco IDs list
+            ids = ids.flatten()
             
-            # Extract the marker corners
-            corners = marker_corner.reshape((4, 2))
-            (top_left, top_right, bottom_right, bottom_left) = corners
+            # Loop over the detected ArUco corners
+            for (marker_corner, marker_id) in zip(corners, ids):
                 
-            # Convert the (x,y) coordinate pairs to integers
-            top_right = (int(top_right[0]), int(top_right[1]))
-            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
-            bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
-            top_left = (int(top_left[0]), int(top_left[1]))
+                # Extract the marker corners
+                corners = marker_corner.reshape((4, 2))
+                (top_left, top_right, bottom_right, bottom_left) = corners
+                    
+                # Convert the (x,y) coordinate pairs to integers
+                top_right = (int(top_right[0]), int(top_right[1]))
+                bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+                bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+                top_left = (int(top_left[0]), int(top_left[1]))
+                    
+                # Draw the bounding box of the ArUco detection
+                cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
+                cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
+                cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
+                cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+                    
+                # Calculate and draw the center of the ArUco marker
+                center_x = int((top_left[0] + bottom_right[0]) / 2.0)
+                center_y = int((top_left[1] + bottom_right[1]) / 2.0)
+                cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
                 
-            # Draw the bounding box of the ArUco detection
-            cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
-            cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
-            cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
-            cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
-                
-            # Calculate and draw the center of the ArUco marker
-            center_x = int((top_left[0] + bottom_right[0]) / 2.0)
-            center_y = int((top_left[1] + bottom_right[1]) / 2.0)
-            cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
-            
-  
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
+    
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
 
-#im = cv2.imread("PINCER_HOUSING2.png")
-#detect_markers(im)
