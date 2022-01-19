612c914720dc496d2b78ea4b7e4b148083d6abc3
ashwin <ashwin@ash-yoga20.04>
2022-01-19 15:57:27 -0500

    pose estimation with solvepnp for AR tag

--- src/pbvs.py
+++ src/pbvs.py
@@ -65,8 +65,15 @@ class PVBS:
                 if(len(corners) >= 4):
                     # pose estimate
                     dist = np.ndarray([0])
-                    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(np.array(corners), 10.0, self.projectionMatrix, dist)
-                #print(out)
+                    print(np.array(marker_corner))
+                  
+                    proj_4x4 = np.array(self.projectionMatrix).reshape(4,4)
+                    proj_3x3 = np.array(self.projectionMatrix).reshape(4,4)[:3, :3]
+                    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corner, 0.183, proj_3x3, 0)
+                    print(rvec)
+                    print(tvec)
+                    cv2.aruco.drawAxis(frame, proj_3x3, 0, rvec[0], tvec[0], 0.3)
+
     
         # Display the resulting frame
         cv2.imshow('frame',frame)
