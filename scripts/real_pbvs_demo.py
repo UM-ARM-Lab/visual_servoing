f71ba1c2e6bec56b91c22698e78c2e128e6e27e1
ashwin <ashwingupta2000@gmail.com>
2022-02-17 17:46:50 -0500

    base frame relative control seems reasonable

--- scripts/real_pbvs_demo.py
+++ scripts/real_pbvs_demo.py
@@ -152,12 +152,10 @@ def main():
                 Tbc = tf.transformations.inverse_matrix(Tcb)
 
                 #  Compute eef target
-                def homo(x):
-                    return np.concatenate((x, [1]), -1)
-                v_base = Tbc @ homo(ctrl[0:3])
+                v_base = Tbc[0:3, 0:3] @ ctrl[0:3]
 
-                # ctrl_base = np.zeros(6)
-                # ctrl_base[0:3] = v_base[0:3]
+                ctrl_base = np.zeros(6)
+                ctrl_base[0:3] = v_base[0:3]
                 # #omega = Tbc @ ctrl[3:6]
                 #
                 # # Compute a jacobian
