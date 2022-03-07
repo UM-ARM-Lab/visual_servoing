526319397adab3eba658a467bea91ba2745c77b7
ashwin <ashwingupta2000@gmail.com>
2022-03-07 17:07:11 -0500

    fixes to pf

--- src/visual_servoing/pf.py
+++ src/visual_servoing/pf.py
@@ -29,11 +29,19 @@ def SE3(se3):
 # developed with rlybrdgs@umich.edu
 class ParticleFilter():
     def __init__(self):
-        self.num_samples = 10
-        self.resampling_noise = 0.05
-        self.sensor_pos_variance = 0.1
-        self.sensor_rot_variance = 0.1 
+        self.num_samples = 1000
+        self.resampling_noise = 0.0004
+        self.sensor_pos_variance = 0.005
+        self.sensor_rot_variance = 5 
         self.is_setup = False
+        self.sensor_cov = np.array([
+                [self.sensor_pos_variance, 0, 0, 0, 0, 0 ], 
+                [0, self.sensor_pos_variance, 0, 0, 0, 0], 
+                [0, 0, self.sensor_pos_variance, 0, 0, 0],
+                [0, 0, 0, self.sensor_rot_variance, 0,  0], 
+                [0, 0, 0, 0, self.sensor_rot_variance, 0], 
+                [0, 0, 0, 0, 0, self.sensor_rot_variance]
+            ]) 
 
     def setup(self, mu_start):
         # particles are N x 6 where each 6 vec represents SE3 pose via <xyz, rod>
@@ -43,7 +51,7 @@ class ParticleFilter():
 
     # sample the sensor reading from a distribution centered at position to get its probability
     def particle_weight(self, particle_pose, sensor_pose):
-        w_pos = scipy.stats.multivariate_normal.pdf(sensor_pose, mean=particle_pose, cov=self.sensor_pos_variance * np.eye(6))
+        w_pos = scipy.stats.multivariate_normal.pdf(sensor_pose, mean=particle_pose, cov=self.sensor_cov)
         return w_pos
 
 
@@ -66,24 +74,27 @@ class ParticleFilter():
         weights /= np.sum(weights)
 
         # use the weighted average of all particles as the state estimate
-        best_estimate = np.average(new_particles, axis=0, weights=weights)
+        #best_estimate = np.average(new_particles, axis=0, weights=weights)
+        # use max weight particle as state estimate        
+        best_estimate = new_particles[np.argmax(weights)]
 
         # resample particles with probability equal to their weights
         idx = np.random.choice(
             weights.shape[0], new_particles.shape[0], list(weights))
 
         # add a small amount of gaussian noise to sampled particles to avoid duplicates
-        noises = np.zeros((self.num_samples, 6)) #  np.random.multivariate_normal(mean=np.zeros(6), cov=np.eye(6)*self.resampling_noise, size=(self.num_samples)) #
+        #noises = np.zeros((self.num_samples, 6)) # 
+        noises = np.random.multivariate_normal(mean=np.zeros(6), cov=np.eye(6)*self.resampling_noise, size=(self.num_samples))  
         resampled_particles = np.vstack(
             [new_particles[i] for i in idx]) + noises
 
         self.particles = resampled_particles 
-        for particle in resampled_particles:
-            particle_mat = SE3(particle)
-            rot = particle_mat[0:3, 0:3]
-            trans = particle_mat[0:3, 3]
-            #draw_pose(trans, rot, mat=True)
-            draw_sphere_marker(trans, 0.05, (1.0, 0.0, 0.0, 1.0)) 
-            print(particle)
+        #for particle in resampled_particles:
+        #    particle_mat = SE3(particle)
+        #    rot = particle_mat[0:3, 0:3]
+        #    trans = particle_mat[0:3, 3]
+        #    #draw_pose(trans, rot, mat=True)
+        #    draw_sphere_marker(trans, 0.05, (1.0, 0.0, 0.0, 1.0)) 
+        #    print(particle)
 
         return SE3(best_estimate)
