e48150709af4ecee95d8911a4feeb71aa8a66493
ashwin <ashwingupta2000@gmail.com>
2022-03-09 17:04:09 -0500

    write file

--- scripts/generate_gt.py
+++ scripts/generate_gt.py
@@ -1,5 +1,8 @@
 # make ground truth point clouds of victor with associated EEF poses
 import numpy as np
+from typing import Dict, Optional, List
+from pathlib import Path
+import gzip
 import pybullet as p
 import pybullet_data
 import cv2 
@@ -7,6 +10,8 @@ from visual_servoing.camera import *
 import open3d as o3d
 from visual_servoing.utils import draw_pose, draw_sphere_marker, erase_pos
 
+from link_bot_data.coerce_types import coerce_types
+import pickle
 client = p.connect(p.GUI)
 #p.setGravity(0, 0, -10)
 urdf = p.loadURDF("models/victor_description/urdf/victor.urdf", [0,0,0], p.getQuaternionFromEuler([0,0,0]))
@@ -23,6 +28,44 @@ for i in range(num_joints):
     joints_by_name[name] = info
     print(f"idx: {info[0]}, joint: {name}, type:{info[2]} ")
 eef_idx = 16
+
+
+def dump_gzipped_pickle(data, filename):
+    while True:
+        try:
+            with gzip.open(filename, 'wb') as data_file:
+                pickle.dump(data, data_file)
+            return
+        except KeyboardInterrupt:
+            pass
+
+def index_to_filename(file_extension, traj_idx):
+    new_filename = f"example_{traj_idx:08d}{file_extension}"
+    return new_filename
+
+def pkl_write_example(full_output_directory, example, traj_idx, extra_metadata_keys: Optional[List[str]] = None):
+    metadata_filename = index_to_filename('.pkl', traj_idx)
+    full_metadata_filename = full_output_directory / metadata_filename
+    example_filename = index_to_filename('.pkl.gz', traj_idx)
+    full_example_filename = full_output_directory / example_filename
+
+    if 'metadata' in example:
+        metadata = example.pop('metadata')
+    else:
+        metadata = {}
+    metadata['data'] = example_filename
+    if extra_metadata_keys is not None:
+        for k in extra_metadata_keys:
+            metadata[k] = example.pop(k)
+
+    metadata = coerce_types(metadata)
+    with full_metadata_filename.open("wb") as metadata_file:
+        pickle.dump(metadata, metadata_file)
+
+    example = coerce_types(example)
+    dump_gzipped_pickle(example, full_example_filename)
+
+    return full_example_filename, full_metadata_filename
 
 while(True):
     p.stepSimulation()
@@ -60,3 +103,5 @@ while(True):
     Twe = Tcw @ Tec
     draw_pose(Twe[0:3, 3], Twe[0:3, 0:3], mat=True) 
 # draw_pose(camera.camera_eye, (np.linalg.inv(camera.get_extrinsics())@Tc1c2 )[0:3, 0:3], mat=True, axis_len=0.1)
+    pkl_write_example(Path("/home/ashwin/Desktop/temp/"), {"a": "a"}, 1) 
+    break
