fix rotation

scripts/dynamics_debug.py
@@ -151,7 +151,7 @@ J = val.get_arm_jacobian("left", True)
 
 pbvs = CheaterPBVS(camera, 1, 1, 1.5, lambda : get_eef_gt(val))
 
-mppi = VisualServoMPPI(dt=0.1, eef_target_pos=Two[0:3, 3])
+mppi = VisualServoMPPI(dt=0.1, eef_target_baselink= np.linalg.inv(val.get_link_pose(0)) @ Two)
 
 
 # DELETE
@@ -173,14 +173,14 @@ while(True):
         q_dot = jac_pinv @ ctrl_limit
         val.velocity_control("left", q_dot, True)
 
-    step = 1
+    step = 5
     if( ctrl_steps % step == 0):
         Twe = get_eef_gt(val)
         cur_joint_config = val.get_joint_states_left() 
         #x = val.get_link_pose(0) @ (mppi.chain.forward_kinematics(cur_joint_config).get_matrix()[0]).cpu().numpy()
         Twe_pred, q_pred = mppi.arm_dynamics_tester(Twe, cur_joint_config, q_dot, val.get_link_pose(0), step)
         Twe_pred_joint = val.get_link_pose(0) @ (mppi.chain.forward_kinematics(q_pred).get_matrix()[0]).cpu().numpy()
-        #eef_pose_pred_vis.update(Twe_pred)
+        eef_pose_pred_vis.update(Twe_pred)
         #eef_pose_joint_vis.update(Twe_pred_joint)
 
     Twe_prev = Twe.copy()
@@ -198,13 +198,14 @@ while(True):
     eef_pose_vis.update(Twe)
     Twc = np.linalg.inv(camera.get_extrinsics())
     ctrl_steps += 1
+
     #camera_pose_vis.update(Twc)
     #target_pose_vis.update(Two)
-    ax, _ = cv2.Rodrigues((Twe @ np.linalg.inv(Twe_prev))[:3, :3])  
+    #ax, _ = cv2.Rodrigues((Twe @ np.linalg.inv(Twe_prev))[:3, :3])  
     #print( f"ground truth: \n ax: {ax / np.linalg.norm(ax)}, angle: {np.linalg.norm(ax)}" ) 
     #print( f"MPPI: \n ax: {mppi.world_axis / torch.linalg.norm(mppi.world_axis)}, angle: {mppi.dt*torch.linalg.norm(mppi.world_axis)}" ) 
-    ax_gt = ax.squeeze()
-    ax_mes = mppi.world_axis.squeeze().numpy() * mppi.dt
-    ax_diff = np.arccos(np.dot(ax_gt, ax_mes) / (np.linalg.norm(ax_gt) * np.linalg.norm(ax_mes))) * 180/np.pi
+    #ax_gt = ax.squeeze()
+    #ax_mes = mppi.world_axis.squeeze().numpy() * mppi.dt
+    #ax_diff = np.arccos(np.dot(ax_gt, ax_mes) / (np.linalg.norm(ax_gt) * np.linalg.norm(ax_mes))) * 180/np.pi
     #print(f"axis variation: {ax_diff} degrees -- magnitude variation { 100* (np.linalg.norm(ax_gt) - np.linalg.norm(ax_mes))/np.linalg.norm(ax_mes)}%")
     #print("next...")

scripts/sandbox.py
@@ -144,7 +144,7 @@ target_pose_vis = PoseVisualizer()
 
 # Target
 Two = np.eye(4) 
-Two[0:3, 3] = np.array([-0.4, 0.5, -0.2])
+Two[0:3, 3] = np.array([-0.2, 0.3, -0.1])
 Two[0:3, 0:3] = np.array(p.getMatrixFromQuaternion(p.getQuaternionFromEuler((-np.pi/2, 0, 0)))).reshape(3, 3) # hard
 
 J = val.get_arm_jacobian("left", True)

src/visual_servoing/mppi_vs.py
@@ -92,9 +92,10 @@ class VisualServoMPPI:
 
         # EEF rotation
         rot_delta = axis_angle_to_quaternion(self.dt * eef_twist[:, 3:])
-        rot_next =  quaternion_multiply(rot, quaternion_invert(rot_delta))
+        #rot_next =  quaternion_multiply(rot, quaternion_invert(rot_delta))
         #rot_next =  quaternion_multiply(rot, quaternion_invert(rot_delta))
         #rot_next =  quaternion_multiply(quaternion_invert(rot_delta), rot)
+        rot_next =  quaternion_multiply(quaternion_invert(rot_delta), rot)
 
         # Update joint config
         q_next = q + u * self.dt
@@ -114,7 +115,7 @@ class VisualServoMPPI:
         delta_rot = quaternion_multiply(x[:, 3:7], quaternion_invert(self.eef_target_rot))
         cost_rot = torch.linalg.norm(quaternion_to_axis_angle(delta_rot), dim=1)
 
-        return cost_pos #+ 0.1*cost_rot
+        return cost_pos + 0.1*cost_rot
 
     def get_control(self, Twe : np.ndarray, Twb : np.ndarray, q : np.ndarray):
         """